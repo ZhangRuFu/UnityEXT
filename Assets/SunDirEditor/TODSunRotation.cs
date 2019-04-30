604acec03e5481bdeaec43724a5d09a284fc97f3
Z.R.F <391536122@qq.com>
2019-04-30 18:54:28 +0800

    添加SunRotationEditor文档
    去掉配置序列化文件的CreateAssetMenu功能

--- Assets/SunDirEditor/TODSunRotation.cs
+++ Assets/SunDirEditor/TODSunRotation.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "SunDir", menuName = "TOD/SunDir")]
 public class TODSunRotation : ScriptableObject
 {
     [System.Serializable]
